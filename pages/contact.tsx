f6fae9bf7cdb99722bfa5892d715b5cf7a2541fd
Tim Liew <74855211+Tweemo@users.noreply.github.com>
2022-06-12 16:35:55 +1200

    added nav and socials to contact

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,13 +6,19 @@ import {
   Box,
 } from '@chakra-ui/react'
 import styles from '../styles/Home.module.css'
+import Socials from './socials'
+import Navbar from './navbar'
 
 const Contact = () => {
   
   return (
+    <>
+    <Navbar />
     <Heading>
       Poop contact
     </Heading>
+    <Socials />
+    </>
     )
   }
 
