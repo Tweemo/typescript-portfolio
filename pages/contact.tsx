24a86d60b421aec1db060d35fd71cdc05ed683bc
Tim Liew <74855211+Tweemo@users.noreply.github.com>
2022-07-08 15:53:03 +1200

    small touch up

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -15,8 +15,8 @@ const Contact = () => {
 return (
     <>
       <Navbar />
-      <Box h={['79.5vh','75vh','70.5vh','66.5vh', '63vh', '61.2vh']}>
-        <Text color='gray' fontSize={['2xl','3xl', '4xl','4xl','5xl']} marginLeft={['20px','20px','40px','180px','200px', '50vh']} marginTop={['20px','40px','76px', '105px']}>
+      <Box h={['79.5vh','75vh','70.5vh','66.5vh', '63vh', '68vh']}>
+        <Text color='gray' fontSize={['2xl','3xl', '4xl','4xl','5xl']} marginLeft={['20px','20px','40px','180px','200px', '30vh']} marginTop={['20px','40px','76px', '105px']}>
           Get In Touch
         </Text>
         <Center>
