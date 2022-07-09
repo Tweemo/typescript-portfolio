c036f644342da009106a6307fbb1ec2d8fdf953d
Tim Liew <74855211+Tweemo@users.noreply.github.com>
2022-07-09 18:39:11 +1200

    fixed view on mac

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import { Text } from '@chakra-ui/react'
  
 const About = () => (
   <>
-    <Text color='gray' fontSize={['xl','2xl','3xl','4xl', '4xl','5xl']} marginLeft={['20px','70px','200px','300px', '900px']} marginBottom={['20px','20px','30px','30px','80px']}>      
+    <Text color='gray' fontSize={['xl','2xl','3xl','4xl', '4xl','5xl']} marginLeft={['20px','70px','200px','300px', '30vh']} marginBottom={['20px','20px','30px','30px','80px']}>      
       About me
     </Text>
     <Text fontSize={['xl','2xl', '3xl','4xl','4xl','5xl']}  h={['28.5vh','30vh','30vh','27vh', '25vh', '30vh']} align='center' margin={['20px','5px', '0px']}>
